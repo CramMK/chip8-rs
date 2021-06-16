2353c575a9b0e401c20e370f372159cdf1ac785b
Marco Thomas <mail@marco-thomas.net>
2021-06-16 11:56:21 +0200

    Add fancy cmd-line parsing

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,4 @@
-extern crate rand;
-extern crate sdl2;
-
-use std::env;
+use clap::{App, Arg};
 
 mod cartridge;
 mod display;
@@ -18,20 +15,32 @@ const SCREEN_WIDTH: usize = 64;
 const SCREEN_HEIGHT: usize = 32;
 const SCREEN_SCALE: usize = 20;
 
-fn main() {
-    let game_file = env::args().nth(1);
-    match game_file {
-        Some(_) => println!("Found a cartridge file! Trying to load..."),
-        None => {
-            println!("No cartridge file found! Exiting!");
-            return;
-        }
-    };
+#[derive(Debug)]
+enum ChipError {
+    CartridgeNotFound,
+}
+
+fn main() -> Result<(), ChipError> {
+    let app = App::new("chip8-rs")
+        .version("0.1.0")
+        .author("Marco Thomas <mail@marco-thomas.net>")
+        .about("A small chip8 emulator, written in Rust")
+        .arg(
+            Arg::with_name("cartridge")
+                .short("c")
+                .takes_value(true)
+                .help("A cartridge binary file"),
+        )
+        .get_matches();
+
+    let game_file = app
+        .value_of("cartridge")
+        .ok_or(ChipError::CartridgeNotFound)?;
 
     let mut processor = Processor::new();
 
-    // load cartridge file
-    let cartridge = Cartridge::new(&game_file.unwrap());
+    let cartridge = Cartridge::new(&game_file);
 
     processor.start(&cartridge.rom);
+    Ok(())
 }
